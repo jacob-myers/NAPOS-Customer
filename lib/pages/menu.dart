ec21a50a45ff439267d64546b4efb93b6259f160
jacob-myers <jakemyers190@gmail.com>
2023-02-02 04:25:35 -0500

    Adding basic framework for the Menu screen. The default screen. App bar with hamburger menu, title, and shopping cart button. Hamburger button opens sidebar with categories.

---
+++ lib/pages/menu.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../widgets/sidebar.dart';
+
+// Using a drawer: https://api.flutter.dev/flutter/material/Scaffold/drawer.html
+// IconButton class: https://api.flutter.dev/flutter/material/IconButton-class.html
+// Icons: https://api.flutter.dev/flutter/material/Icons-class.html
+
+class MyMenu extends StatefulWidget {
+  const MyMenu({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyMenu> createState() => _MyMenuState();
+}
+
+class _MyMenuState extends State<MyMenu> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  void _openDrawer() {
+    _scaffoldKey.currentState!.openDrawer();
+  }
+
+  void _closeDrawer() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: _openDrawer
+        ),
+        title: Center(
+            child: Text(widget.title)
+        ),
+        actions: [
+          // Amount of money in cart (not implemented).
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              "\$1.12",
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          //Button to navigate to cart.
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              // NAVIGATE TO CART
+            },
+          )
+        ],
+      ),
+      drawer: SideBar(),
+    );
+  }
+}
---
+++ lib/widgets/sidebar.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+// App bar documentation: https://api.flutter.dev/flutter/material/AppBar-class.html
+// Create a sidebar: https://oflutter.com/create-a-sidebar-menu-in-flutter/
+
+class SideBar extends StatefulWidget {
+  const SideBar({super.key});
+
+
+  @override
+  State<SideBar> createState() => _SideBar();
+}
+
+class _SideBar extends State<SideBar> {
+  void _closeDrawer() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Scaffold(
+        // Categories appBar
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: _closeDrawer,
+          ),
+          title: const Text("Categories"),
+        ),
+        // Categories Body
+        body: ListView(
+          children: [
+            InkWell(
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text("Food")
+                )
+              ),
+              onTap: () {
+
+              },
+            ),
+            InkWell(
+              child: Card(
+                  child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text("Drink")
+                  )
+              ),
+              onTap: () {
+
+              },
+            ),
+            InkWell(
+              child: Card(
+                  child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text("Merchandise")
+                  )
+              ),
+              onTap: () {
+
+              },
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
